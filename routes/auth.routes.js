d480b7373255f1036032bf8bd3d805920840c579
sultzx <quangaliyev@gmail.com>
2022-12-30 20:25:10 +0500

    last

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,11 +2,11 @@ import express from 'express'
 
 import * as controller from '../controllers/auth.controllers.js'
 import * as validation from '../services/validations.js'
-import handleValidations from '../services/handleValidations.js'
+import validationHandler from '../services/validationHandler.js'
 
 const authRouter = express.Router()
 
-authRouter.post('/registration', validation.registration, handleValidations, controller.registration)
-authRouter.post('/login', validation.login, handleValidations, controller.login)
+authRouter.post('/registration', validation.registration, validationHandler, controller.registration)
+authRouter.post('/login', validation.login, validationHandler, controller.login)
 
 export default authRouter
