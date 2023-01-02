6e74cd101465ecc8c373168b0c03932ecfec33c3
sultzx <quangaliyev@gmail.com>
2023-01-03 00:42:18 +0500

    last

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -8,8 +8,10 @@ import User from '../models/User.js'
 export const registration = async (req, res) => {
     try {
         
+        const { email,  password } = req.body
+
         const isEmailExist = await User.findOne({
-            email: req.body.email
+            email: email
         })
 
         if (isEmailExist) {
@@ -19,10 +21,10 @@ export const registration = async (req, res) => {
         }
 
         const salt = await bcrypt.genSalt(6)
-        const hash = await bcrypt.hash(req.body.password, salt)
+        const hash = await bcrypt.hash(password, salt)
 
         const document = new User({
-            email: req.body.email,
+            email: email,
             hashedPassword: hash
         })
 
@@ -44,10 +46,45 @@ export const registration = async (req, res) => {
     }
 }
 
-export const login =(req, res) => {
+export const login = async (req, res) => {
     try {
         
+        const { email, password } = req.body
+
+        const user = await User.findOne({
+            email: email
+        })
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Қолданушы желіде жоқ'
+            })
+        }
+
+        const isPassValid = await bcrypt.compare( req.body.password, user._doc.hashedPassword )
+
+        if (!isPassValid) {
+            return res.status(400).json({
+                message: 'Құпия сөз қате терілген'
+            })
+        }
+
+        const token = jwt.sign({
+            _id: user._id
+        }, config.get('jwt_key'), {
+            expiresIn: '1h'
+        })
+
+        const { hashedPassword, ...userData } = user._doc
+
+        res.status(200).json({
+            ...userData,
+            token
+        })
+
     } catch (error) {
-        
+        res.status(500).json({
+            message: error.message
+        })
     }
 }
--- services/validations.js
+++ services/validations.js
@@ -2,10 +2,10 @@ import { body } from 'express-validator'
 
 export const registration = [
     body('email').isEmail(),
-    body('password').isLength({ min: 6, max: 16 }).isString()
+    body('password').isLength({ min: 5, max: 16 }).isString()
 ]
 
 export const login = [
-    body('email', '').isEmail(),
-    body('password', '').isLength({ min: 6, max: 16 }).isString()
+    body('email').isEmail(),
+    body('password').isLength({ min: 5, max: 16 }).isString()
 ]
