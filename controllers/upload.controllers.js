066b0f7bf2fd5318e6d3c845a268a0cdfa5e080a
sultzx <quangaliyev@gmail.com>
2023-02-03 01:15:26 +0600

    last

--- controllers/upload.controllers.js
+++ controllers/upload.controllers.js
@@ -1,5 +1,6 @@
 
 import User from "../models/User.js"
+import News from "../models/News.js"
 
 export const uploadAvatar = async (req, res) => {
     const url = `/uploads/avatars/${req.file.originalname}`
@@ -18,9 +19,20 @@ export const uploadFiles = (req, res) => {
         url: `/uploads/files/${req.file.originalname}`
     })
 }
+ 
+export const uploadNewsImages = async (req, res) => {
+    const url = `/uploads/newspaper/${req.file.originalname}`
+    const id = JSON.parse(JSON.stringify(req.body)).id
+    
+    if (id) {
+         await News.updateOne({
+        _id: id
+        }, {
+            imageUrl: `http://localhost:4444${url}`
+        })
+    }
 
-export const uploadNewsImages = (req, res) => {
     res.json({
-        url: `/uploads/newspaper/${req.file.originalname}`
+        url: url
     })
-}
+}  
